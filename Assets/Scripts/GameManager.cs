213e9fc9a2efed3c7d3e167fae67206501912bdd
HSanchez1018 <89468338+HSanchez1018@users.noreply.github.com>
2022-09-27 18:22:57 -0400

    Added coin,hazard, and scoreboard

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public int score = 0;
 
-    public GameObject itemPrefab;
+    public GameObject [] itemPrefab;
     public Transform leftTran;
     public Transform rightTran;
+
+    public TextMeshPro scoreText;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +22,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Score: " + score.ToString(); 
     }
 
 
@@ -29,7 +33,13 @@ public class GameManager : MonoBehaviour
 
         Vector2 spawnPos = new Vector2(rndXValue, leftTran.position.y);
 
-        Instantiate(itemPrefab, spawnPos, Quaternion.identity);
+        int index = Random.Range(0, itemPrefab.Length);
+        Instantiate(itemPrefab[index], spawnPos, Quaternion.identity);
 
+    }
+
+    public void IncrementScore(int value)
+    {
+        score += value;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 
 {
+    public GameManager gm;
     public float speed = 5;
 // Start is called before the first frame update
 void Start()
@@ -21,5 +22,25 @@ void Start()
         //Debug.Log("xMove:" + xMove);
         transform.Translate(xMove * speed * Time.deltaTime, 0, 0);
      
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+        //Debug.Log("Collided"); 
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            gm.IncrementScore(1);
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Hazard")
+        {
+            Destroy(gameObject);
+        }
+        
+        
     }
 }
